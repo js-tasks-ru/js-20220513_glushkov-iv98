e81e7567738b7c6b638f4386e8b2bd77cdc01fcc
oryndoon <oryndoon@student.21-school.ru>
2022-05-19 16:37:53 +0300

    sort array of strings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,19 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  //для корректной сортировки Uppercase
+  const sortOptions = {
+    caseFirst: 'upper',
+  };
+  //для корректной сортировки смешанных локалей
+  const locales = ['ru', 'en'];
 
+  //возвращаем копию отсортированного массива
+  return [...arr].sort(function (a, b) {
+    if (param === 'asc') {
+      return a.localeCompare(b, locales, sortOptions);
+    } else {
+      return b.localeCompare(a, locales, sortOptions);
+    }
+  });
 }
