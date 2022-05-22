7bc27b36da2c72873c6f8593a1740d780eb1cfd0
oryndoon <oryndoon@student.21-school.ru>
2022-05-22 17:36:21 +0300

    trimSymbols - removes consecutive identical symbols if they quantity bigger that size

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,31 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  let finalString = [];
+  let currentPos = 0;
+  const stringSize = string.length;
 
+  if (size === undefined) {
+    return string;
+  }
+  while (currentPos < stringSize) {
+    currentPos += pushSolidSequence(string[currentPos]);
+  }
+
+  function pushSolidSequence(symbol) {
+    let sequenceLength = 0;
+
+    while (string[currentPos] === symbol) {
+      if (sequenceLength < size) {
+        finalString.push(string[currentPos]);
+      }
+      sequenceLength++;
+      currentPos++;
+    }
+
+    //если длинна неповторяющейся цепочки символов === 1, то сдвигаем каретку на 1. Иначе сдвигаем на sequenceLength
+    return sequenceLength === 0 ? 1 : sequenceLength;
+  }
+
+  return finalString.join('');
 }
