7abb26682682f9f025386fc89a2bd869530ae588
oryndoon <oryndoon@student.21-school.ru>
2022-06-03 08:09:11 +0300

    small refactoring

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -10,6 +10,7 @@ export default class SortableTable {
 
   constructor(headerConfig = [], data = []) {
     this.headerConfig = headerConfig;
+    this.data = data;
     this.columItems = this.headerConfig.map((item) => {
       return {
         id: item.id,
